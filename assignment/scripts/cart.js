02184bd3b0bc8ec4a53a3e5bc1902c07de329b0a
Chris Harwood <cmharwood79@gmail.com>
2024-04-10 16:14:07 -0500

    through required

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -4,13 +4,49 @@ console.log('***** Cart Functions *****');
 
 let basket = [];
 
+function listItems() {
+  if (basket.length === 0) {
+    console.log('Basket is empty');
+  }
+  for (let item of basket) {
+    console.log(item);
+  }
+}
+console.log('List of basket items:');
+listItems();
+
 function addItem(item) {
-    
+  basket.push(item);
+  return true;
+}
+console.log(`Basket is ${basket}`);
+console.log('Adding milk', addItem('milk'));
+console.log(`Basket is now ${basket}`);
+console.log('Adding waffles', addItem('waffles'));
+console.log(`Basket is now ${basket}`);
+console.log('Adding pizza', addItem('pizza'));
+console.log(`Basket is now ${basket}`);
+
+console.log('List of basket items:');
+listItems();
+
+// console.log(basket);
+
+function empty() {
+  basket.length = 0; // Is it better to set the property to 0 or reset it?
+  //basket.splice(0, basket.length);
 }
 
+console.log(`Basket is ${basket}`);
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log(`Basket is now ${basket}`);
 
+console.log('Basket before emptying:');
+listItems();
 
-
+console.log('Basket after emptying:');
+empty();
+console.log(`Basket is now ${basket}`);
 
 // DO NOT MODIFY
 // Used for automated testing
