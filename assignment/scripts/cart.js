cf481c5aa7df513e154955db83fb82d1b774f629
Chris Harwood <cmharwood79@gmail.com>
2024-04-10 11:32:49 -0500

    just getting started

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,11 +2,11 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = [];
 
-
-
-
-
+function addItem(item) {
+    
+}
 
 
 
@@ -15,15 +15,15 @@ console.log('***** Cart Functions *****');
 // DO NOT MODIFY
 // Used for automated testing
 try {
-    module.exports = {
-        basket: typeof basket !== 'undefined' ? basket : undefined,
-        addItem: typeof addItem !== 'undefined' ? addItem : undefined,
-        listItems: typeof listItems !== 'undefined' ? listItems : undefined,
-        maxItems: typeof maxItems !== 'undefined' ? maxItems : undefined,
-        empty: typeof empty !== 'undefined' ? empty : undefined,
-        isFull: typeof isFull !== 'undefined' ? isFull : undefined,
-        removeItem: typeof removeItem !== 'undefined' ? removeItem : undefined,
-    };
-} catch(e) {
-    // Do nothing
+  module.exports = {
+    basket: typeof basket !== 'undefined' ? basket : undefined,
+    addItem: typeof addItem !== 'undefined' ? addItem : undefined,
+    listItems: typeof listItems !== 'undefined' ? listItems : undefined,
+    maxItems: typeof maxItems !== 'undefined' ? maxItems : undefined,
+    empty: typeof empty !== 'undefined' ? empty : undefined,
+    isFull: typeof isFull !== 'undefined' ? isFull : undefined,
+    removeItem: typeof removeItem !== 'undefined' ? removeItem : undefined,
+  };
+} catch (e) {
+  // Do nothing
 }
